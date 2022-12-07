1a6004cd7554fd81c9f628e1510b031272532067
baltikaa9 <sergey4016@mail.ru>
2022-12-07 21:40:05 +0300

    Добавлена разность чисел.

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@ int main() {
     cin >> b;
 
     cout << endl << "Сумма чисел = " << a + b << endl;
+    cout << endl << "Разность чисел = " << a - b << endl;
 
     system("pause");
     return 0;
